e2399e75760ad31eaa91b3fc4e6b83b8f0d8d389
Mira Heckmann <miraheckmann@gmail.com>
2020-10-17 12:42:15 -0700

    adds type info

--- js/script.js
+++ js/script.js
@@ -1,29 +1,25 @@
 //alert('Js is working');
 
 //Constants
-const BASE_URL = 'https://pokeapi.co/api/v2/pokemon'; 
+const BASE_URL = 'https://pokeapi.co/api/v2/pokemon';
 
 
 //Variables
-let pokemonData, pokemonDetail; 
-let pokemonTeam = []; 
-let spritesArr = []; 
+let pokemonData, pokemonDetail;
+let pokemonTeam = [];
+let spritesArr = [];
 
 //Caches Element References
-const $listEl = $('#pokemon-list'); 
-const $sprite = $('#sprite');
-const $cardsEl = $('#cards'); 
-const $sprite2 = $('#sprite2');
-const $sprite3 = $('#sprite3');
-const $sprite4 = $('#sprite4');
-const $sprite5 = $('#sprite5');
-const $sprite6 = $('#sprite6');
-//sprites have to be an array? 
+const $listEl = $('#pokemon-list');
+const $cardsEl = $('#cards');
+const $typeEl = $('#types'); 
+
+
 //Event Listeners
 $listEl.on('click', 'li', handleClick)
 //Functions 
 
-init(); 
+init();
 
 function init() {
     getData();
@@ -32,63 +28,81 @@ function init() {
 function getData(detailURL) {
     const url = detailURL ? detailURL : BASE_URL;
 
-    $.ajax(url) 
-    .then(function(data) {
-        if(detailURL) {
-            pokemonDetail = data; 
-            //console.log('Data: ', data);   
-            render(true); 
-        } else {
-            pokemonData = data; 
-            render(); 
-            //console.log('Data: ', data);   
-        }
-    }, function(error) {
-        console.log('Error: ', error); 
-    });
+    $.ajax(url)
+        .then(function (data) {
+            if (detailURL) {
+                pokemonDetail = data;
+                //console.log('Data: ', data);   
+                render(true);
+            } else {
+                pokemonData = data;
+                render();
+                //console.log('Data: ', data);   
+            }
+        }, function (error) {
+            console.log('Error: ', error);
+        });
 
 }
 
 function handleClick() {
     const url = this.dataset.url;
-    getData(url); 
+    getData(url);
     //clicking on a name on the list turns it into the photo of the charater that was clicked and then stores the name in an array that will be called later when looking up the team types 
-    
+
 }
 
 
 function generateList() {
-    return pokemonData.results.map(function(pokemon) {
+    return pokemonData.results.map(function (pokemon) {
         return `
         <li data-url="${pokemon.url}">
             ${pokemon.name}
         </li>
-        `; 
-    }); 
+        `;
+    });
 }
 
 
 
 
 function render(isDetail) {
-    if(isDetail) {
-        pokemonTeam.push(pokemonDetail.types); 
-        console.log(pokemonTeam); 
+    if (isDetail) {
+        //save type data in an arary 
+        if (pokemonTeam.length < 6) {
+            pokemonTeam.push(pokemonDetail.types);
+            //console.log(pokemonTeam);
+        } else { 
+            return; 
+        } 
+        pokemonTeam.forEach(function (pokemon) {
+            pokemon.forEach(function(typeStat) {
+                let allTypes = [];
+                allTypes = typeStat.type.name; 
+                console.log(allTypes); 
+                $typeEl.text(`Types: ${allTypes}`)
+                //console.log('Types: ', typeStat.type.name)
+            });
+        });
 
-        let pokeSprite = pokemonDetail.sprites.front_default; 
-        spritesArr.push(pokeSprite); 
-        console.log(spritesArr); 
-
-        spritesArr.forEach(function(image){
-            var img = document.createElement('img');
-            img.src = image;
-            $cardsEl.append(img);
-            spritesArr = [];  
+        //save sprite data in an array
+        let pokeSprite = pokemonDetail.sprites.front_default;
+        spritesArr.push(pokeSprite);
+        //console.log("spites Array", spritesArr);
+        //render individual sprites to the screen 
+        spritesArr.forEach(function (image) {
+            if (pokemonTeam.length < 7) {
+                var img = document.createElement('img');
+                img.src = image;
+                $cardsEl.append(img);
+                spritesArr = [];
+            } else {
+                return;
+            }
         })
-   
-     } else {
+
+    } else {
         $listEl.html(generateList())
-       
+
     }
-} 
- 
+}
